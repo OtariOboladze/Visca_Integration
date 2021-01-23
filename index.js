d0d96374c47f91bd54e614533649669d3bebca09
Otari Oboladze <otari.oboladze@outlook.com>
2021-01-23 11:53:37 +0100

    minor changes

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ $.ajax({
     url : folder,
     success: function (data) {
         $(data).find("a").attr("href", function (i, val) {
-            if( val.match(/\.(jpe?g|png|gif)$/) ) { 
+            array.forEach(val => {
+                
+            });( val.match(/\.(jpe?g|png|gif)$/) ) { 
                 $("#logo").append( "<img src='"+ folder + val +"'>" );
             } 
         });
